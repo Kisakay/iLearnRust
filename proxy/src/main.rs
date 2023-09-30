9e032e645651bc8203934b46b1028b441b97349d
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-09-30 18:52:16 +0200

    Update main.rs

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,17 +1,19 @@
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn handle_client(stream: TcpStream) {
+    
 
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:888").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:16384").unwrap();
     loop {
         for stream in listener.incoming() {
             println!("{:?}", listener);
-            handle_client(stream.unwrap());
+            thread::spawn(||{
+                handle_client(stream.unwrap())
+            });
         }
-
     }
-    
 }
