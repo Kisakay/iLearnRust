0f05c7e98f7947df953e59bbd526e7054f69cb46
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-10-14 15:45:50 +0200

    Update main.rs

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,19 +1,38 @@
+use std::io::prelude::*;
+use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-fn handle_client(stream: TcpStream) {
-    
-
+fn handle_client(mut stream: TcpStream) {
+    loop {
+        let packet: Vec<u8> = Vec::new();
+        loop {
+            let raw_usize_read_data = stream.read(&mut [0; 128]).unwrap();
+            let raw_u8_read_data: &[u8] = &raw_usize_read_data.to_ne_bytes()[..];
+            let utf8_read_data = std::str::from_utf8(raw_u8_read_data);
+            if utf8_read_data.is_ok() {
+                println!("{:?}", utf8_read_data.unwrap());
+                if utf8_read_data.unwrap().contains("\0") {
+                    break;
+                }
+            }
+        }
+        //stream.write(a);
+        //println!("{:?}", a);
+    }
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:16384").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:16384").unwrap();
     loop {
-        for stream in listener.incoming() {
-            println!("{:?}", listener);
-            thread::spawn(||{
-                handle_client(stream.unwrap())
-            });
+        match listener.accept() {
+            Ok((_socket, addr)) => {
+                println!("new client: {addr:?}");
+                thread::spawn(|| {
+                    handle_client(_socket);
+                });
+            }
+            Err(e) => println!("couldn't get client: {e:?}"),
         }
     }
 }
